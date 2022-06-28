08992e98d735490ff6ed7ad570d3dba1db45f242
ariel1031 <null>
2022-06-28 02:21:01 +0000

    DAY1 – 자료형

--- DATA_TYPE_EX6.py
+++ DATA_TYPE_EX6.py
@@ -1,5 +1,5 @@
 # • 문제 : ‘abcdefg123123123aaa’ a 문자를 ‘A’로 변경하기
-# • 문자열 타입 변수 선언 및 초기화, replace 함수 활용하기
+# • 문자열 타입 변수 선언 및 초기화, replace 함수 활용하기 
 
 str1 = str('abcdefg123123123aaa')
 str2 = str1.replace("a", "A")
