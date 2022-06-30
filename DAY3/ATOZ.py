b9186b0020da3b96c21141c218ee601eb616009b
ariel1031 <null>
2022-06-30 04:48:18 +0000

    도전 과제

---
+++ DAY3/ATOZ.py
@@ -0,0 +1,12 @@
+#s = input('문자열을 입력하세요 : ')
+s = input('문자열을 입력하세요 : ')
+pos = 'abcdefghijklmnopqrstuvwxyz'
+
+for i in pos:
+    if i in s:
+        print(s.index(i), end=' ')
+    else:
+        print(-1, end = ' ')
+
+
+
