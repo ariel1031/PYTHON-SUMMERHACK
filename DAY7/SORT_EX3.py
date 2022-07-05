52da25e65b4d0ab71d4619a3f05a09ed4b1a0efa
ariel1031 <null>
2022-07-05 06:44:35 +0000

    DAY7

--- DAY7/SORT_EX3.py
+++ DAY7/SORT_EX3.py
@@ -3,12 +3,14 @@
 # • 참고 : 나이가 같으면 먼저 가입한 사람이 앞에 온다.
 # • 요구사항 : 시간 제한 1초, 메모리 제한 128MB
 
+# 람다, 소티드
+# https://velog.io/@aonee/Python-%EC%A0%95%EB%A0%AC-sort-sorted-reverse
 
 #기본 방식 1(람다 키 사용)
 n_members = int(input('회원의 인원 : '))
 member_list = []
 
-for _ int range(n_members):
+for _ in range(n_members):
     (x, y) = input('나이와 이름을 공백으로 입력 받습니다.').split()
     member_list.append((int(x), y)) 
     
